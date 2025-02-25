835e30b10898a16d188c2bda1dfd63d8adb9f8df
Pashikanti <pruthvik@corp.untd.com>
2025-02-25 15:14:00 +0530

    Created initial project

---
+++ Angular_Auth/AngularAuthAPI/AngularAuthAPI/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AngularAuthAPI.Models
+{
+    public class User
+    {
+        [Key]
+        public int UserId { get; set; }
+        public string Username { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Token {  get; set; }
+        public string Role { get; set; }
+        public string RefreshToken { get; set; }
+        public DateTime RefreshTokenExpireTime { get; set; }
+    }
+}
