d6b0a949c348f591ad8d266cf459de23e8558763
Pashikanti <pruthvik@corp.untd.com>
2025-03-07 17:31:18 +0530

    Revert "Include files"
    This reverts commit e2ff86eb3798f65e81d2bc4981fe9c9bffd8d9b9.

--- AngularAuthAPI/AngularAuthAPI/DTO/TokenApiDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace AngularAuthAPI.DTO
-{
-    public partial class TokenApiDto
-    {
-        public string RefreshToken { get; set; } = string.Empty;
-        public string AccessToken { get; set; } = string.Empty;
-    }
-}
--- AngularAuthAPI/AngularAuthAPI/Database/AuthDb.cs
+++
@@ -1,9 +0,0 @@
-﻿using AngularAuthAPI.Models;
-
-namespace AngularAuthAPI.Database
-{
-    public class AuthDb
-    {
-        public static List<User> Users { get; set; } = new();
-    }
-}
